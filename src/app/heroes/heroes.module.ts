025c62c03ed7109d703be8f8832857ec1d493550
Fernando Nicolas <fersanti2896@gmail.com>
2022-03-07 02:03:49 -0600

    Agregando @angular/flex-layout@12.0.0 y @angular/cdk@11.2.13 a las dependencias

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { HeroesRoutingModule } from './heroes-routing.module';
 
 import { AgregarComponent } from './pages/agregar/agregar.component';
@@ -18,6 +19,7 @@ import { ListadoComponent } from './pages/listado/listado.component';
   ],
   imports: [
     CommonModule,
+    FlexLayoutModule,
     HeroesRoutingModule
   ]
 })
