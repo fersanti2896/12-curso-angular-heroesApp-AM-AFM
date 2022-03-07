5a0f60548a66efc34a7d683c49be08f43ded976f
Fernando Nicolas <fersanti2896@gmail.com>
2022-03-07 13:10:13 -0600

    Aplicando material sidenav, toolbar e iconos a home.component

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button'
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
-  declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    MatSidenavModule
+  ], 
+  exports: [
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatToolbarModule
   ]
 })
 export class MaterialModule { }
