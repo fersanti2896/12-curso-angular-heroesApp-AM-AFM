e8ecb8b78ec0bcc66dd41a34cb756bd7468047a5
Fernando Nicolas <fersanti2896@gmail.com>
2022-03-14 01:13:54 -0600

    Mostrando mensaje cuando en el buscador no concuerde con el heroe en buscar.component

--- src/app/heroes/pages/buscar/buscar.component.ts
+++ src/app/heroes/pages/buscar/buscar.component.ts
@@ -12,7 +12,7 @@ import { HeroesService } from '../../services/heroes.service';
 export class BuscarComponent implements OnInit {
   termino: string = '';
   heroes: Heroe[] = []
-  heroeSeleccionado!: Heroe;
+  heroeSeleccionado: Heroe | undefined;
 
   constructor( private heroesService: HeroesService ) { }
 
@@ -20,14 +20,19 @@ export class BuscarComponent implements OnInit {
   }
 
   buscando() {
-    this.heroesService.getSugerencias(this.termino)
+    this.heroesService.getSugerencias(this.termino.trim())
         .subscribe(heroes => this.heroes = heroes);
   }
 
   opcionSeleccionada(event: MatAutocompleteSelectedEvent) {
-    const heroe: Heroe = event.option.value;
-    this.termino = heroe.superhero;
+    if(!event.option.value) {
+      this.heroeSeleccionado = undefined;
+      return;
+    } 
 
+    const heroe: Heroe = event.option.value;
+    
+    this.termino = heroe.superhero;
     this.heroesService.getHeroeById(heroe.id!)
         .subscribe(heroe => this.heroeSeleccionado = heroe);
   }
